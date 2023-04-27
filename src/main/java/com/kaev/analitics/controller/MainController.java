777d4e101ba00075a42bbe35962b5a01c982a399
attilakiszelik <attila.kiszelik@gmail.com>
2023-04-27 23:04:51 +0200

    add pathvariable to controllers

--- src/main/java/com/kaev/analitics/controller/MainController.java
+++ src/main/java/com/kaev/analitics/controller/MainController.java
@@ -2,8 +2,11 @@ package com.kaev.analitics.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,15 +25,23 @@ public class MainController {
 	private final UsageService usageService;
 	
 	@CrossOrigin(origins = "http://localhost:4200")
-	@GetMapping("/supporthours")
-	public List<ReportRow> getSupporthours(){
-		return supporthourService.createReport("2023");
+	@GetMapping("/supporthours/{year}")
+	public List<ReportRow> getSupporthours(@PathVariable String year){
+		return supporthourService.createReport(year);
 	}
 	
 	@CrossOrigin(origins = "http://localhost:4200")
-	@GetMapping("/usages")
-	public List<ReportRow> getUsages(){
-		return usageService.createReport("2023");
+	@GetMapping("/usages/{year}")
+	public List<ReportRow> getUsages(@PathVariable String year){
+		return usageService.createReport(year);
 	}
+	
+    @GetMapping("/ip")
+    public String getIpAddress(HttpServletRequest request) {
+    
+	    String containersIP = request.getRemoteAddr();
+	    return "the container's IP address is: " + containersIP;
+    
+    }
 	
 }
